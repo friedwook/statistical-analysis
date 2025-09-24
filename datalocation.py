9400cb64d9cb62d588c9afaff474199d3652cfed
friedwook <yoojw1118@gmail.com>
2025-09-24 16:40:40 +0900

    9월 24일 수업 내용

--- datalocation.py
+++ datalocation.py
@@ -2,3 +2,4 @@ import os
 
 currentdir = os.path.dirname(os.path.abspath(__file__))
 ch2_scores_em_loc = os.path.join(currentdir, r"source\python_stat_sample-master\data\ch2_scores_em.csv")
+anscombe = os.path.join(currentdir, r"source\python_stat_sample-master\data\ch3_anscombe.npy")
